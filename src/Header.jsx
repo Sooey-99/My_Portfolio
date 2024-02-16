5d222288f4e51111480108aec8692bce89f20ece
Émile Siou <104312278+Sooey-99@users.noreply.github.com>
2024-02-16 20:01:30 +0000

    Adding a Header.jsx file

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>Émile Siou's Portfolio</h1>
+            <nav>
+                <ul>
+                    <li><Home href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    )
+}
+
+export default Header
